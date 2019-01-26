3a2a4db475ebec1d799bcedeac1615cc40701132
Joe Ganobsik <jganobsik@gmail.com>
2019-01-26 20:31:23 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,8 +4,11 @@ class Helpers
     @user = User.find_by_id(session[:user_id])
   end
   
-  def is_logged_in?
-     !!session[:user_id]
+  def is_logged_in?(session)
+     if session[:user_id] != nil
+       true 
+     else false 
+     end
      
   end
 end
