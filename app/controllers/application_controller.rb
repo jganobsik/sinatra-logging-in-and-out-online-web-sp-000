d5b06b6be35ad7819aea7b48b2dd96edf87e490d
Joe Ganobsik <jganobsik@gmail.com>
2019-01-26 20:26:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
   session[:user_id] = @user.id
- @user = User.find_by(:username => params[:username])
+  @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
